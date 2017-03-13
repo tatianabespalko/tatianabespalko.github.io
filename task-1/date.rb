8890136aacbcb4e502ec872508a653cb782b131f
Tanya <tbespalko87@gmail.com>
2017-03-14 00:21:20 +0200

    add week

---
+++ task-1/date.rb
@@ -0,0 +1,16 @@
+require "date"
+# Посмотрим, в какую неделю попадает 1 мая в 2002 и 2005 годах.
+t1 = Time.local(2016,3,13)
+d1 = Date.new(2016,3,13)
+week1a = t1.strftime("%U").to_i # 11
+week1b = t1.strftime("%W").to_i # 11
+week1c = d1.cweek #10
+t2 = Time.local(2005,5,1)
+d2 = Date.new(2005,5,1)
+week2a = t2.strftime("%U").to_i # 18
+week2b = t2.strftime("%W").to_i # 18
+week2c = d2.cweek               # 17
+
+Time
+t0 = Time.new
+Time.now: t0 = Time.now
