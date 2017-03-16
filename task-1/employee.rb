c1ae39b3abd901a5c45b788c37779c0d3aff4b80
Tanya <tbespalko87@gmail.com>
2017-03-17 01:03:01 +0200

    add new file employee.rb

--- task-1/employee.rb
+++ task-1/employee.rb
@@ -7,8 +7,11 @@ class Employee
 		end
 		@name = name # Cохранить имя в переменной екземпляра
 	end
-	def print_name
-		puts "Name: #{name}"
+	def initialize (name = "Anonymous")
+    	self.name = name
+    end
+    def print_name
+    	puts "Name: #{name}"
 	end
 end
 class SalariedEmployee < Employee
@@ -20,6 +23,45 @@ class SalariedEmployee < Employee
 		end
 		@salary = salary
 	end
+	def initialize (name = "Anonymous", salary = 0.0)
+		super(name)
+		self.salary = salary
+	end
+	def print_pay_stub
+		print_name
+		pay_for_period = (salary / 365.0) * 14
+		formatted_pay = format("$%.2f", pay_for_period)
+		puts "Pay This Period: #{formatted_pay}"
+	end
+end
+class HourlyEmployee < Employee
+	def self.security_guard(name)
+		HourlyEmployee.new(name, 19.25, 30)
+	end
+	def self.cashier(name)
+		HourlyEmployee.new(name, 12.75, 25)	
+	end
+	def self.janitor(name)
+		HourlyEmployee.new(name, 10.5, 20)
+	end
+	attr_reader :hourly_wage, :hours_per_week
+	def hourly_wage=(hourly_wage) # Код проверки и присваивания @hourly_wage
+		if hourly_wage < 0
+			raise "An hourly wage of #{hourly_wage} isn't valid"
+		end
+		@hourly_wage = hourly_wage
+	end
+	def hours_per_week=(hours_per_week) # Код проверки и присваивания @hours_per_week
+		if hours_per_week < 0
+			raise "#{hours_per_week} hours per week isn't valid"
+		end
+		@hours_per_week = hours_per_week
+	end
+	def initialize (name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+		super(name)
+		self.hourly_wage = hourly_wage
+		self.hours_per_week = hours_per_week
+	end	
 	def print_pay_stub
 		print_name
 		pay_for_period = hourly_wage * hours_per_week * 2
@@ -27,19 +69,6 @@ class SalariedEmployee < Employee
 		puts "Pay This Period: #{formatted_pay}"
 	end
 end
-class HourlyEmployee < Employee
-	attr_reader :hourly_wage, :hours_per_week
-	def hourly_wage=(hourly_wage) # Код проверки и присваивания @hourly_wage
-		@hourly_wage = hourly_wage
-	end
-	def hours_per_week=(hours_per_week)
-	# Код проверки и присваивания @hours_per_week
-	    @hours_per_week = hours_per_week
-	end
-end
-
-	#def print_pay_stub
-	#		print_name
 	#		pay_for_period =(salary/365.0) * 14 # вычислить 14-дневную часть оклада работника.
 	#		formatted_pay = format("%.2f", pay_for_period) # Получение строки с денежной суммой, округленной до двух цифр в дробной части 
 	#		puts "Pay This Period: $#{formatted_pay}" # Вывести начисленную сумму (отформатированная)
@@ -91,8 +120,20 @@ end
 #salaried_employee.salary = 50000
 #salaried_employee.print_pay_stub
 
-hourly_employee = HourlyEmployee.new
-hourly_employee.name = "John Smith"
-hourly_employee.hourly_wage = 14.97
-hourly_employee.hours_per_week = 30
+#hourly_employee = HourlyEmployee.new
+#hourly_employee.name = "John Smith"
+#hourly_employee.hourly_wage = 14.97
+#hourly_employee.hours_per_week = 30
+#hourly_employee.print_pay_stub
+
+salaried_employee = SalariedEmployee.new("Jane Doe", 50000)
+salaried_employee.print_pay_stub
+hourly_employee = HourlyEmployee.new("John Smith", 14.97, 30)
 hourly_employee.print_pay_stub
+
+jane = SalariedEmployee.new("Jane Doe", 50000)
+jane.print_pay_stub
+angela = HourlyEmployee.security_guard("Angela Matthews")
+ivan = HourlyEmployee.cashier("Ivan Stokes")
+angela.print_pay_stub
+ivan.print_pay_stub
