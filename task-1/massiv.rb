6d832efbbb487d8cabef0e3fc87bc7718b0215d9
Tanya <tbespalko87@gmail.com>
2017-03-18 20:04:49 +0200

    add file massiv.rb

---
+++ task-1/massiv.rb
@@ -0,0 +1,44 @@
+prices = [2.99, 25.00, 9.99]
+prices[0]
+prices[1]
+prices[2]
+puts prices[0]
+puts prices[2]
+puts prices[1]
+
+
+#prices = [7.99, 25.00, 3.99, 9.99]
+#puts prices.class
+
+#puts prices.first
+#puts prices.last
+#puts prices.length
+#puts prices.include?(25.00)
+#puts prices.find_index(9.99)
+
+#prices.push(0.99)
+#p prices
+
+#prices.shift
+#p prices
+
+
+#prices << 5.99
+#prices << 8.99
+#p prices
+
+#prices.pop
+#p prices
+
+#puts ["d", "o", "g"].join
+#puts ["d", "o", "g"].join("-")
+
+#p "d-o-g".chars
+
+#p "d-o-g".split("-")
+
+index = 0
+while index < prices.length
+	puts prices[index]
+	index += 1
+end
