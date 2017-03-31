8a4deda5d9b9d7a8461e7488dfb20cc49652c36b
Tanya <tbespalko87@gmail.com>
2017-03-31 23:02:17 +0300

    try add rdoc

---
+++ task-1/word_splitter.rb
@@ -0,0 +1,18 @@
+#This class allows you to perform various operations on the words in a string.
+class WordSplitter
+	include Enumerable
+	# The string to split into words.
+	attr_accessor :string
+	# Passes each word in the string to a block one at a time.
+	def each
+		string.split(" ").each do |word|
+			yield word
+		end
+	end
+	# Creates a new instance with its string attribute set to the given string.
+	def initialize(string)
+		self.string = string
+	end
+end
+
+
