4fc76c16847dd848b54a024ded483ad2b0235c7b
Tanya <tbespalko87@gmail.com>
2017-05-18 00:17:31 +0300

    add

--- task_ruby/Глава_7/candidate.rb
+++ task_ruby/Глава_7/candidate.rb
@@ -1,10 +1,16 @@
 class Candidate
 	attr_accessor :name, :age, :occupation, :hobby, :birthplace
-	def initialize(name, age, occupation, hobby = nil, birthplace)
+	#def initialize(name, age, occupation, hobby = nil, birthplace)
+	def initialize(name, age: nil, occupation: nil, hobby: nil, birthplace: "Sleepy Creek")
+	#def initialize(name, options ={})
 		self.name = name
+		#self.age = options [:age]
 		self.age = age
+		#self.occupation = options [:occupation]
 		self.occupation = occupation
+		#self.hobby = options [:hobby]
 		self.hobby = hobby
+		#self.birthplace = options [:birthplace]
 		self.birthplace = birthplace
 	end
 end
@@ -17,5 +23,10 @@ def print_summary(candidate)
 	puts "Birthplace: #{candidate.birthplace}"
 end
 
-candidate = Candidate.new("Carl Barnes", 49, "Attorney", "Miami")
-print_summary(candidate)
+#candidate = Candidate.new("Carl Barnes", 49, "Attorney", "Miami")
+#candidate = Candidate.new("Amy Nguyen", age: 37, occupation: "Engineer", hobby: "Lacrosse", birthplace: "Seattle")
+#candidate = Candidate.new("Amy Nguyen", age: 37, occupation: "Engineer")
+#print_summary(candidate)
+
+p Candidate.new("Amy Nguyen", age: 37, occupation: "Engineer")
+p Candidate.new("Carl Barnes")
