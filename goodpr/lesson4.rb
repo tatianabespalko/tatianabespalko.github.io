79272953e20f7914028897cf73501517a5ae4f4a
Tanya <tbespalko87@gmail.com>
2017-05-19 20:11:21 +0300

    renoved files

---
+++ goodpr/lesson4.rb
@@ -0,0 +1,18 @@
+#puts "Я обьект типа строка!".length - выводит количество символов в строке
+#puts 17 - выводит значение
+#puts 17.to_f # метод переводит в обьект с плавающей точкой
+
+hello = "Привет, девченки!"
+hello2 = nil #   = это присвоение значения
+
+#hello2 = hello
+hello2 = "пока, пацаны"
+patsany = nil
+
+if (patsany != nil) # != не равно
+	puts hello2
+else
+	puts hello
+end
+#puts hello
+#puts hello2
