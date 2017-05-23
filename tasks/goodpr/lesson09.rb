0bae6e0ea3e38ed7714c2ab7aebee2393436fdcd
Tanya <tbespalko7@gmail.com>
2017-05-24 00:02:07 +0300

    add tasks to lesson

---
+++ tasks/goodpr/lesson09.rb
@@ -0,0 +1,27 @@
+#puts "Я строка!"
+#puts = 1
+#puts puts
+
+
+#puts "Я строка 1"
+#puts
+#puts("Я строка 2")
+#array = ["James Bond", "Brad Pit", "Dr. Who", "House MD", "Madonna"]
+#puts array.to_s
+#array.pop 
+#puts array.to_s
+#array.pop 2
+#puts array.to_s
+
+
+#time = Time.now  # Присваиваем переменной time значение, который возвращает метод
+#puts time
+
+#def имя_метода(параметр1, параметр2, ...) ... end
+
+def  say_hi(name)
+	puts "Привет, #{name}, как дела?"
+end
+
+say_hi "Вадим"
+say_hi "Таня"
