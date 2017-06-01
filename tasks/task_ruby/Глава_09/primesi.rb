4ae3033bafcc3e040579f7a17b31b98123f42274
TBespalko <tbespalko7@gmail.com>
2017-06-01 17:44:01 +0300

    add chapter 9 ruby

---
+++ tasks/task_ruby/Глава_09/primesi.rb
@@ -0,0 +1,55 @@
+# 2) 
+module AcceptsComments
+	def comments
+		if @comments
+			@comments
+		else
+			@comments = []
+		end
+	end
+	def add_comment (comment)
+		comments << comment
+	end
+end
+class Clip
+	#attr_reader :comments
+	#1)
+	#def initialize
+	#	@comments = []
+	#end
+	
+	def play
+		puts "Playing #{object_id}..."
+	end
+end
+
+class Video < Clip
+	include AcceptsComments
+	attr_accessor:resolution
+end
+
+class Song < Clip
+	include AcceptsComments
+	attr_accessor :beats_per_minute
+end
+class Photo
+	include AcceptsComments
+	def show
+		puts "Displaying #{object_id}..."
+	end
+end
+
+video = Video.new
+video.add_comment("Cool slow motion effect!")
+video.add_comment("Weird ending.")
+song = Song.new
+song.add_comment("Awesome beat.")
+photo = Photo.new
+photo.add_comment("Beatiful colors.")
+p video.comments, song.comments, photo.comments
+
+video.play
+song.play
+photo.show
+
+
