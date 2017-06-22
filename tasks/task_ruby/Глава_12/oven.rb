1a55dcc231987d55540a744f0b2848c3f38f58d7
TBespalko <tbespalko7@gmail.com>
2017-06-22 22:24:41 +0300

    add chapter 12

--- tasks/task_ruby/Глава_12/oven.rb
+++ tasks/task_ruby/Глава_12/oven.rb
@@ -1,3 +1,9 @@
+# 2
+class OvenOffError < StandardError
+end
+class OvenEmptyError < StandardError
+end
+
 class SmallOven
 	attr_accessor :contents
 	def turn_on
@@ -10,19 +16,43 @@ class SmallOven
 	end
 	def bake
 		unless @state == "on"
-			return "You need to turn the oven on first!"
+			#2
+			raise OvenOffError, "You need to turn the oven on first!"
+			#1) return "You need to turn the oven on first!"
 		end
 		if @contents == nil
-			return "There's nothing in the oven!"
+			raise OvenEmptyError, "There's nothing in the oven!"
+			#1) return "There's nothing in the oven!"
 		end
 		"golden-brown #{contents}"
 	end
 end
 
-dinner = ['turkey', 'casserole', 'pie']
+#dinner = ['turkey', 'casserole', 'pie']
+dinner = ['turkey', nil, 'pie']
 oven = SmallOven.new
-oven.turn_on
-dinner.each do |item|
-	oven.contents = item
-	puts "Serving #{oven.bake}."
+oven.turn_off
+  dinner.each do |item|
+  	begin
+  		#oven.contents = item
+  		#oven.contents = 'turkey'
+  		oven.contents = nil
+  		puts "Serving #{oven.bake}."
+  		#rescue => error
+  	rescue OvenEmptyError => error
+  		puts "Error: #{error.message}"
+	rescue OvenOffError => error
+		oven.turn_on
+		retry
+	ensure
+		oven.turn_off
+	end
 end
+	# 2) oven.turn_off - духовка выключена
+	#oven.contents = 'turkey'
+	#puts "Serving #{oven.bake}."
+
+	#oven = SmallOven.new
+	#oven.turn_on
+	#oven.contents = nil
+	#puts "Serving #{oven.bake}."
