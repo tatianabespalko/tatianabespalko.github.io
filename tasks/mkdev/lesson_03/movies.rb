89937b4064bb0721f9f8260f66573220b563ea3d
TBespalko <tbespalko7@gmail.com>
2017-09-28 21:48:29 +0300

    add_task_2_mkdev

--- tasks/mkdev/lesson_03/movies.rb
+++ tasks/mkdev/lesson_03/movies.rb
@@ -14,49 +14,43 @@
 # method chaining это написание кода в одну строчку, использование разных методов последовательно в цепочке.
 # https://www.sitepoint.com/a-guide-to-method-chaining/
 
-# Считать из файла все фильмы
-input = ARGV.join
-filename = "movies.txt"
+input = ARGV.first 
 
-lines = []
-File.open("movies.txt") do |movie_file|
-	review_file = File.open("movies.txt") 
-	lines = movie_file.readlines
-	
-end
-# Хранить каждый фильм в словаре (Hash) — ключами такого словаря будут, например, :link, :title, и т.д. — по смыслу полей в файле
-@movies = {}
-lines.each do |value|
-  	value = value.split("|")
-  	@movies[value[1]] =
-	  	{ link: value[0],
-	      title: value[1],
-	      year: value[2],
-	      country: value[3],
-	      release: value[4],
-	      genres: value[5],
-	      time: value[6],
-	      rating: value[7],
-	      director: value[8],
-	      starring: value[9]
-	    }
+input || input = "movies.txt" 
+
+
+# array
+FIELDS = %i[ link name year country release_date genre length rating director actors ]
+
+# splitted array of movies, IO.foreach reads file line-by-line processing by block
+array_of_hashes = IO.foreach(input).map { |x| FIELDS.zip(x.chomp.to_s.split("|")).to_h}
+
+def star_rating (rating) # convert float rating into stars
+  "*"*(((rating - 8)*10)+1)
 end
 
-def search(str, field = :title)
-	filter = @movies.select do |key, value|
-		value[field].downcase.include? str.downcase
-	end
-
-# 5 самых длинных фильмов;
-
-# Рейтинг нужно вывести количеством звёздочек. Поскольку у всех фильмов в топ-250 рейтинг больше 8,
-#то количество звёздочек должно соответствовать десятым долям рейтинга выше восьмёрки (для рейтинга «8.6»
-#должна рядом с названием быть выведена строка ******, для 9.2 — соответственно, ************)
-	filter.each do |key, value|
-	    rating = ((value[:rating].to_f.ceil - value[:rating].to_f)*10).to_i
-	    puts "#{value[:title]} #{"".ljust(rating, "*")}"
-	end
+def nice_output (arr)
+  arr.each { |x| puts "#{x[:name]} (#{x[:release_date]}; #{x[:genre]}) - #{x[:length]} "}
 end
 
+# 5 самых длинных фильмов
+arr_five_longest = array_of_hashes.max_by(5) {|key| key[:length].to_i}
 
-search "Max"
+# 10 комедий (первые по дате выхода)
+arr_ten_comedies = array_of_hashes.find_all {|key| key[:genre].include?("Comedy")}.max_by(10) {|x| x[:release_date]}
+
+# список всех режиссёров по алфавиту (без повторов!) — кстати, стоит отсортировать их по фамилии (будем считать таковой последнее слово ФИО);
+directors = array_of_hashes.map { |x| x[:director]}.uniq.sort_by { |x| x.split.last }
+
+# количество фильмов, снятых не в США.
+number_of_nonusa = array_of_hashes.reject { |x| x[:country].include? ("USA")}.count
+
+# Вывести результат
+puts "\n\n5 longest movies:\n#{"*"*70}"
+nice_output(arr_five_longest)
+puts "\n\n10 most recent comedies\n#{"*"*70}"
+nice_output(arr_ten_comedies)
+puts "\n\nList of Directors in alphabetical order\n#{"*"*70}"
+directors.each { |x| print "#{x}; "}
+puts "\n\nNumber of non-USA movies: #{number_of_nonusa}\n\n"
+puts "\n\n"
